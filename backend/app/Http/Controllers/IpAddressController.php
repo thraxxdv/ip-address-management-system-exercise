3e225ca6e6bdf4f6d799da3217c34d002c619b83
Dylan Villaruel <thraxxdv@gmail.com>
2024-06-27 23:22:07 +0800

    feat: initial skeletons for ip address module

---
+++ backend/app/Http/Controllers/IpAddressController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\IpAddress;
+use Illuminate\Http\Request;
+
+class IpAddressController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(IpAddress $ipAddress)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, IpAddress $ipAddress)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(IpAddress $ipAddress)
+    {
+        //
+    }
+}
